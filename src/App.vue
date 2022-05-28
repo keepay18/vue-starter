01d80aa3c98711af70c45ef10ff66d8ea0a292b8
student35 <student35@pcoip.ki.agh.edu.pl>
2022-05-28 13:56:13 +0200

    Wyrazenia logiczne

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
   <div>
     <h1>Twój email to: {{email}}</h1>
     <input type="text" v-model="email">
+    <div v-if="email.length < 10">Ale masz krótki adres!</div>
+    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
+    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
+
 
   </div>
 </template>
