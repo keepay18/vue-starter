15254c7ed953817f6adfe1f1a3927e003ffa9494
student35 <student35@pcoip.ki.agh.edu.pl>
2022-05-28 15:54:03 +0200

    Zrobione co na zajeciach

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,10 @@
     <div v-if="!email">
       
       <login-form @login="enter($event)" button-label="Wejdź"></login-form>
-      <login-form @login="enter($event)" button-label="Wleć"></login-form>
-      <login-form @login="enter($event)" :button-label="Math.random() < 0.5 ? 'Etykieta A' : 'Etykieta B'"></login-form>                                                                                                                                         
+                                                                                                                                            
     </div>
     <div v-else>
-      <h2>Witaj {{email}}</h2>
-      <a @click="logOut()">Wyloguj</a>
+      <log-out-form :email="email" @logout="logOut()"></log-out-form>
     </div>
     
 
@@ -24,8 +22,10 @@
 <script>
 import "milligram"
 import LoginForm from "./LoginForm";
+import LogOutForm from "./LogOutForm";
+
 export default {
-  components: {LoginForm},
+  components: {LoginForm, LogOutForm},
   data(){
     return{
       email: '',
---
+++ src/LogOutForm.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>Witaj {{email}}</h2>
+        <a @click="logOut()">Wyloguj</a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['email'],
+    methods: {
+        logOut(){
+            this.$emit('logout')
+        }
+    }
+}
+</script>
